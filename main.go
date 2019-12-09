3ccfce8083358c6b56460ba995d479fcfd3516f0
Tom Brookson <tombrookson97@gmail.com>
2019-12-09 18:21:34 +0000

    day 2 part 2

--- main.go
+++ main.go
@@ -46,7 +46,7 @@ func opcodeTwo(pos1 int, pos2 int, pos3 int, program []int) {
 	program[pos3] = program[pos1] * program[pos2]
 }
 
-func main() {
+func part1() {
 	opcodes, err := parseFile(defaultFilename)
 	if err != nil {
 		fmt.Println(err)
@@ -69,4 +69,43 @@ func main() {
 	}
 
 	fmt.Println(opcodes)
+}
+
+func part2() {
+	for noun := 0; noun <= 99; noun++ {
+		for verb := 0; verb <= 99; verb++ {
+			opcodes, err := parseFile(defaultFilename)
+			if err != nil {
+				fmt.Println(err)
+			}
+
+			opcodeLines := splitOpcodes(opcodes)
+
+			opcodeLines[0][1] = noun
+			opcodeLines[0][2] = verb
+
+			for _, opcodeLine := range opcodeLines {
+				optype := opcodeLine[0]
+
+				if optype == 1 {
+					opcodeOne(opcodeLine[1], opcodeLine[2], opcodeLine[3], opcodes)
+				}
+				if optype == 2 {
+					opcodeTwo(opcodeLine[1], opcodeLine[2], opcodeLine[3], opcodes)
+				}
+				if optype == 99 {
+					break
+				}
+			}
+
+			if opcodes[0] == 19690720 {
+				answer := (100 * opcodes[1]) + opcodes[2]
+				fmt.Println("Works! Answer: ", answer)
+			}
+		}
+	}
+}
+
+func main() {
+	part2()
 }
